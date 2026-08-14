5e9a76f8968c6bbeab3771b8c12ec9584dd26d63
Ariel Ruiz <ruizgarcesariel@gmail.com>
2026-08-14 10:16:51 -0500

    Updates

---
+++ assets/current-progression-bg-038e8419.js
@@ -0,0 +1,1 @@
+const r="/rc-calculator/assets/current-progression-bg-4367a845.png";export{r as default};
--- assets/current-progression-bg-e6446e3c.js
+++
@@ -1,1 +0,0 @@
-const r="/rc-calculator/assets/current-progression-bg-cbb0e041.png";export{r as default};
